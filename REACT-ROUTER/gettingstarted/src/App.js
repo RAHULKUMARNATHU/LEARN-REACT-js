ccb9995b200bcd6e35e73ff65f299df01a955255
RAHUL KUMAR <rahulkumars9572@gmail.com>
2022-10-14 22:57:07 +0530

    commit ::done with getting started

--- REACT-ROUTER/gettingstarted/src/App.js
+++ REACT-ROUTER/gettingstarted/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./component/Home";
+import About from "./component/About";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Welcome To react-router-dom</h2>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/about" element={<About/>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ REACT-ROUTER/gettingstarted/src/component/About.js
@@ -0,0 +1,6 @@
+function About(){
+    return<>
+    <h2>About Page</h2>
+    </>
+}
+export default About;
