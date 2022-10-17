b49c0ccbac7ccc8b7866491e283ef7422918e5c3
RAHUL KUMAR <rahulkumars9572@gmail.com>
2022-10-17 15:06:05 +0530

    commit ::done with useNavigate

--- React-Router-Hooks/usenavigative/src/components/Home.js
+++ React-Router-Hooks/usenavigative/src/components/Home.js
@@ -1,7 +1,26 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link ,useNavigate} from 'react-router-dom'
 
 export default function Home() {
+  const navigate = useNavigate();
+
+
+// const navToPage=(bool)=>{
+//   let x = bool;
+//   if(x){
+//     navigate('/user')
+//   }else{
+//     navigate('/filter')
+//   }
+ 
+// }
+
+
+const navToPage=(url)=>{
+  navigate(url)
+
+}
+
   return (
     <div>
       <h2>
@@ -15,11 +34,15 @@ export default function Home() {
       </p>
       <Link to="/contact">Go to Contact</Link>
       <br/>
-      <button>
+      {/* <button onClick={()=>navigate('/user')} > */}
+      {/* <button onClick={()=>navToPage(true)}> */}
+      <button onClick={()=>navToPage('/user')}>
       Go to User
-      </button>
+      </button >
       <br/>
-      <button>
+      {/* <button onClick={()=>navigate('/filter')} > */}
+      {/* <button onClick={()=>navToPage(false)}> */}
+      <button onClick={()=>navToPage('/filter')}>
       Go to Filter
       </button>
     </div>
